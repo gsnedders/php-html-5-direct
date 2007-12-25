87771a399da32ca2df7b4d7623f47165aec27797
Geoffrey Sneddon <geoffers@gmail.com>
2007-12-25 22:25:22 +0000

    Redo the tester (yay! something that properly works!).

--- src/numbersTest.php
+++ src/numbersTest.php
@@ -1,94 +1,98 @@
 <?php
+// Call numbersTest::main() if this source file is executed directly.
+if (!defined('PHPUnit_MAIN_METHOD')) {
+	define('PHPUnit_MAIN_METHOD', 'numbersTest::main');
+}
 
 require_once 'PHPUnit/Framework.php';
-require_once 'PHPUnit/Util/Timer.php';
 
 require_once 'numbers.php';
 
-class numbersTest implements PHPUnit_Framework_Test
+/**
+ * Test class for numbers.
+ */
+class numbersTest extends PHPUnit_Framework_TestCase
 {
-	private $tests = array();
-	
-	public static function suite()
+	/**
+	 * @var numbers
+	 */
+	protected $object;
+
+	/**
+	 * Runs the test methods of this class.
+	 */
+	public static function main()
 	{
-		$suite = new PHPUnit_Framework_TestSuite;
-		$suite->addTest(new numbersTest());
-		return $suite;
+		require_once 'PHPUnit/TextUI/TestRunner.php';
+
+		$suite	= new PHPUnit_Framework_TestSuite('numbersTest');
+		$result = PHPUnit_TextUI_TestRunner::run($suite);
 	}
 	
-	public function __construct($data_file = '../../tests/numbersTest')
-	{
-		$this->tests = json_decode(file_get_contents($data_file), true);
-	}
-	
-	public function count()
-	{
-		return 1;
-	}
-	
-	public function getName()
-	{
-		return __CLASS__;
-	}
-	
-	public function run(PHPUnit_Framework_TestResult $result = null)
-	{
-		if ($result === null)
+	/**
+	 * Loads the data from a the JSON file into a structure that can be read by
+	 * PHPUnit.
+	 *
+	 * @return array
+	 */
+	public static function numbersProvider()
+    {
+    	$return = array();
+		$tests = json_decode(file_get_contents('../tests/numbersTest'), true);
+		foreach ($tests as $test => $all_expected)
 		{
-			$result = new PHPUnit_Framework_TestResult;
-		}
-		
-		$methods = array('unsigned', 'signed', 'real_numbers', 'ratios', 'percentages_and_dimensions', 'lists');
-		
-		foreach ($this->tests as $test => $expectations)
-		{
-			reset($methods);
-			foreach ($expectations as $expect)
+			foreach ($all_expected as $expected)
 			{
-				$result->startTest($this);
-				PHPUnit_Util_Timer::start();
-				
-				try
-				{
-					if (is_int($expect) || is_array($expect))
-					{
-						PHPUnit_Framework_Assert::assertEquals($expect, call_user_func(array('numbers', current($methods)), $test));
-					}
-					else
-					{
-						PHPUnit_Framework_Assert::assertSame($expect, call_user_func(array('numbers', current($methods)), $test));
-					}
-				}
-				catch (PHPUnit_Framework_AssertionFailedError $e)
-				{
-					$result->addFailure($this, $e, PHPUnit_Util_Timer::stop());
-				}
-				catch (Exception $e)
-				{
-					if ($e->getCode() === numbers::unspecified)
-					{
-						try
-						{
-							throw new PHPUnit_Framework_IncompleteTestError($e->getMessage());
-						}
-						catch (Exception $e2)
-						{
-							$result->addError($this, $e2, PHPUnit_Util_Timer::stop());
-						}
-					}
-					else
-					{
-						$result->addError($this, $e, PHPUnit_Util_Timer::stop());
-					}
-				}
-				next($methods);
-		
-				$result->endTest($this, PHPUnit_Util_Timer::stop());
+				$return[] = array($test, $expected);
 			}
 		}
-
-		return $result;
+		return $return;
+    }
+ 
+    /**
+     * @dataProvider numbersProvider
+     */
+	public function testNumbers($test_data, $expected)
+	{
+		static $methods = array('unsigned', 'signed', 'real_numbers', 'ratios', 'percentages_and_dimensions', 'lists');
+		$method = current($methods);
+		
+		if (!next($methods))
+		{
+			reset($methods);
+		}
+		
+		try
+		{
+			if (is_int($expected) || is_array($expected))
+			{
+				$this->assertEquals($expected, call_user_func(array('numbers', $method), $test_data));
+			}
+			else
+			{
+				$this->assertSame($expected, call_user_func(array('numbers', $method), $test_data));
+			}
+		}
+		catch (Exception $e)
+		{
+			if ($e->getCode() === numbers::unspecified)
+			{
+				$this->markTestIncomplete($e->getMessage());
+			}
+			else
+			{
+				$this->fail($e->getMessage());
+			}
+		}
 	}
+	
+	
+}
+
+// Call numbersTest::main() if this source file is executed directly.
+if (PHPUnit_MAIN_METHOD === 'numbersTest::main')
+{
+	numbersTest::main();
 }
 
 ?>
